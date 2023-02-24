bab28d351fcdfb832087319880681e014ab4503d
Guilherme Oliveira <guilherme.061@gmail.com>
2023-02-24 14:32:16 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #define GUEEPO2D_MAIN
 #include <gueepo2d.h>
 
-class Sample : public gueepo::Application {
+class GBGJ2 : public gueepo::Application {
 public:
-	Sample() : Application("sample ", 640, 360) {}
-	~Sample() {}
+    GBGJ2() : Application("goblin bunker!!", 640, 360) {}
+	~GBGJ2() {}
 };
 
 gueepo::Application* gueepo::CreateApplication() {
-	return new Sample();
+	return new GBGJ2();
 }
